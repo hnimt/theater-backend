99e14268cd3f463f0099ba37567524e378e53be0
hnimt <tongminh23996@gmail.com>
2022-07-02 23:15:28 +0700

    add is pay invoice

--- app/invoice/admin.py
+++ app/invoice/admin.py
@@ -8,6 +8,42 @@ class InvoiceAdmin(admin.ModelAdmin):
         model = Invoice
 
     readonly_fields = ['id', 'user', 'tax', 'total']
+    list_display = ['id', 'user', 'get_movie', 'get_room', 'get_show_date', 'get_show_time', 'is_pay']
+    list_display_links = ['id', 'user']
+    search_fields = ['user__email']
+    list_filter = ['is_pay']
+
+    @admin.display(description='movie')
+    def get_movie(self, obj):
+        schedule_seats = obj.schedule_seats.all()
+        if len(schedule_seats) > 0:
+            return schedule_seats[0].schedule_movie.movie.title
+        else:
+            return '<Blank>'
+
+    @admin.display(description='room')
+    def get_room(self, obj):
+        schedule_seats = obj.schedule_seats.all()
+        if len(schedule_seats) > 0:
+            return schedule_seats[0].schedule_movie.room.name
+        else:
+            return '<Blank>'
+
+    @admin.display(description='show date')
+    def get_show_date(self, obj):
+        schedule_seats = obj.schedule_seats.all()
+        if len(schedule_seats) > 0:
+            return schedule_seats[0].schedule_movie.show_date.value
+        else:
+            return '<Blank>'
+
+    @admin.display(description='show time')
+    def get_show_time(self, obj):
+        schedule_seats = obj.schedule_seats.all()
+        if len(schedule_seats) > 0:
+            return schedule_seats[0].schedule_movie.show_time.value
+        else:
+            return '<Blank>'
 
 
 admin.site.register(Invoice, InvoiceAdmin)
--- app/invoice/models.py
+++ app/invoice/models.py
@@ -9,6 +9,7 @@ class Invoice(ModelBase):
     tax = models.DecimalField(default=constants.TAX, max_digits=10, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
     user = models.ForeignKey(get_user_model(), related_name="invoices", on_delete=models.CASCADE)
+    is_pay = models.BooleanField(default=False)
 
     def __str__(self):
         return f"User: {self.user} | Total: {self.total}$"
